80599e1dc565e235c10329d95c385b62f4a142c7
rendra <r3ndr412@gmail.com>
2025-06-20 21:55:43 +0700

    tes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "https://euphoriarr.github.io/radiant-aura-gleam/",
   server: {
     host: "::",
     port: 8080,
