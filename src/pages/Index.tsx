62e6d48362360f8d42c6d6e213683e2281fb1de0
rendra <r3ndr412@gmail.com>
2025-06-20 15:05:28 +0700

    komit 1

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,16 +10,23 @@ import Chatbot from '@/components/Chatbot';
 
 const Index = () => {
   return (
-    <div className="min-h-screen max-w-[1440px] mx-auto">
-      <Header />
-      <HeroSection />
-      <Features />
-      <CaseStudies />
-      <Pricing />
-      <FinalCTA />
-      <Footer />
-      <Chatbot />
-    </div>
+    // Ganti kode lama Anda dengan yang ini di index.tsx
+<div className="flex flex-col min-h-screen">
+  <Header />
+
+  {/* Konten utama sekarang dibungkus oleh <main> yang menjadi container */}
+  <main className="w-full flex-1 max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+    <HeroSection />
+    <Features />
+    <CaseStudies />
+    <Pricing />
+    
+  </main>
+  <FinalCTA />
+  <Footer />
+  
+  {/* Chatbot biasanya memiliki posisi fixed/absolute, jadi penempatannya di sini sudah baik */}
+</div>
   );
 };
 
