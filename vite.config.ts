bf2f11938b0b894de8d7df4ebf95d4596c55d9f6
EuphoriaRR <61897881+EuphoriaRR@users.noreply.github.com>
2025-06-20 15:37:23 +0700

    Update vite.config.ts
    nope

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "https://github.com/EuphoriaRR/radiant-aura-gleam",
   server: {
     host: "::",
     port: 8080,
